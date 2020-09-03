90ff78c9f3e9972567a1ed7c8b4a952a9d2c8586
jerichocalibuso <jerichocalibuso@gmail.com>
2020-09-04 03:12:47 +0800

    section 9

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,46 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+// This interface tells other classes on how they can be an argument for addMarker method
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(id: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(id) as Element,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
